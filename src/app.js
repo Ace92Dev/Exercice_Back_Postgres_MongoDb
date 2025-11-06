48b300d52eeda16cc3fc6eacc8717f1651f00de0
Ace <ace92dev@gmail.com>
2025-11-06 14:34:41 +0100

    adds routes

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/pg/tasks', pgTaskRoutes);
 app.use('/mongo/tasks', mongoTaskRoutes);
+app.use('/pg/todos', pgTaskRoutes);
+app.use('/mongo/todos', mongoTaskRoutes);
 
 module.exports = app;
 
